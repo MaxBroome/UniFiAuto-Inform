f7472447c28f8eeb7853d699b05555a638489506
Max Broomfield <56701581+MaxBroome@users.noreply.github.com>
2023-04-12 23:32:14 -0500

    Proper Ping Sweep

--- Unifi-ARP.ps1
+++ Unifi-ARP.ps1
@@ -1,8 +1,18 @@
 $macPrefixes = "00-15-6D", "00-27-22", "00-50-C2", "04-18-D6", "18-E8-29", "24-5A-4C", "24-A4-3C", "28-70-4E", "44-D9-E7", "60-22-32", "68-72-51", "68-D7-9A", "70-A7-41", "74-83-C2", "74-AC-B9", "78-45-58", "78-8A-20", "80-2A-A8", "94-2A-6F", "9C-05-D6", "AC-8B-A9", "B4-FB-E4", "D0-21-F9", "D8-B3-70", "DC-9F-DB", "E0-63-DA", "E4-38-83", "F0-9F-C2", "F4-92-BF", "F4-E2-C6", "FC-EC-DA"
 
+$subnets = Read-Host "Please enter the first 3 octets of your subnet (e.g. 192.168.1)"
+
 Write-Host "Searching for Ubiquiti Devices..." -ForegroundColor Yellow
 
-Start-Job -ScriptBlock { arp /a } | Out-Null
+$pingJob = Start-Job -ScriptBlock {
+    param($subnet)
+    $Subnet = "$subnet."
+    1..254 | ForEach-Object {
+        ping.exe -n 1 -l 0 -f -i 2 -w 1 -4 "$Subnet$_" | Out-Null
+    }
+} -ArgumentList $subnets
+
+Wait-Job $pingJob | Out-Null
 
 $arpOutput = arp -a | Select-String -Pattern "([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}"
 
