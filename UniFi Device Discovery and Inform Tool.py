7b42661ee4caa52aa8e5c027dda1c8bfbed36cc5
Max Broomfield <56701581+MaxBroome@users.noreply.github.com>
2023-06-05 20:35:27 -0500

    Switched from arp -a to nmap
    Switched from using arp -a which is windows only, to nmap for its performance and reliability (mostly). GUI Version is next if all goes well with this release!

--- UniFi Device Discovery and Inform Tool.py
+++ UniFi Device Discovery and Inform Tool.py
@@ -1,133 +1,57 @@
 import socket
-import subprocess
-import re
 import paramiko
 import time
-from threading import Thread, Lock
-from time import perf_counter
+import nmap
 from colorama import *
 
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 init(convert=True)
 
-print(f"{bcolors.UNDERLINE}Searching for Ubiquiti Devices...{bcolors.ENDC}")
+# Get the user's LAN IP to use for the nmap scan.
+# This can be weird sometimes, and only works for /24 subnets. I will add an override for this in the coming GUI release.
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("1.1.1.1", 80))
+local_ip = s.getsockname()[0]
+split_ip = local_ip.rsplit(".", 1)
+target_ip = split_ip[0] + ".0/24"
 
-BASE_IP = "%s.%i"
-PORT = 8080
-
-class Threader:
-    def __init__(self, threads=30):
-        self.thread_lock = Lock()
-        self.functions_lock = Lock()
-        self.functions = []
-        self.threads = []
-        self.nthreads = threads
-        self.running = True
-
-    def stop(self) -> None:
-        self.running = False
-
-    def append(self, function, *args) -> None:
-        self.functions.append((function, args))
-
-    def start(self) -> None:
-        for i in range(self.nthreads):
-            thread = Thread(target=self.worker, daemon=True)
-            thread._args = (thread, )
-            self.threads.append(thread)
-            thread.start()
-
-    def join(self) -> None:
-        for thread in self.threads:
-            thread.join()
-
-    def worker(self, thread:Thread) -> None:
-        while self.running and (len(self.functions) > 0):
-            with self.functions_lock:
-                function, args = self.functions.pop(0)
-            function(*args)
-
-        with self.thread_lock:
-            self.threads.remove(thread)
-
-start = perf_counter()
-socket.setdefaulttimeout(1.0)
-
-def connect(hostname, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        result = sock.connect_ex((hostname, port))
-
-def get_gateway_octets():
-    result = subprocess.run('ipconfig', capture_output=True, text=True)
-    output = result.stdout
-
-    gateways = re.findall(r'IPv4 Address[^:]+:\s+(\d+\.\d+\.\d+)\.\d+', output)
-    return gateways
-
-# Retrieve gateways
-gateways = get_gateway_octets()
-
-total_ips = len(gateways) * 255
-n_threads = min(total_ips, 30)
-
-threader = Threader(n_threads)
-for gateway in gateways:
-    for i in range(255):
-        threader.append(connect, BASE_IP % (gateway, i), PORT)
-        if len(threader.functions) >= n_threads:
-            threader.start()
-            threader.join()
-
-threader.start()
-threader.join()
-
-end = perf_counter()
+print(f"Searching for Ubiquiti Devices in {bcolors.HEADER}{target_ip}{bcolors.ENDC}...")
 
 mac_prefixes = [
-    "00-15-6D", "00-27-22", "00-50-C2", "04-18-D6", "18-E8-29", "24-5A-4C",
-    "24-A4-3C", "28-70-4E", "44-D9-E7", "60-22-32", "68-72-51", "68-D7-9A",
-    "70-A7-41", "74-83-C2", "74-AC-B9", "78-45-58", "78-8A-20", "80-2A-A8",
-    "94-2A-6F", "9C-05-D6", "AC-8B-A9", "B4-FB-E4", "D0-21-F9", "D8-B3-70",
-    "DC-9F-DB", "E0-63-DA", "E4-38-83", "F0-9F-C2", "F4-92-BF", "F4-E2-C6",
-    "FC-EC-DA"
+    "00:15:6D", "00:27:22", "00:50:C2", "04:18:D6", "18:E8:29", "24:5A:4C",
+    "24:A4:3C", "28:70:4E", "44:D9:E7", "60:22:32", "68:72:51", "68:D7:9A",
+    "70:A7:41", "74:83:C2", "74:AC:B9", "78:45:58", "78:8A:20", "80:2A:A8",
+    "94:2A:6F", "9C:05:D6", "AC:8B:A9", "B4:FB:E4", "D0:21:F9", "D8:B3:70",
+    "DC:9F:DB", "E0:63:DA", "E4:38:83", "F0:9F:C2", "F4:92:BF", "F4:E2:C6",
+    "FC:EC:DA"
 ]
 
-arp_output = subprocess.check_output("arp -a").decode()
+# Also for some reason, sometimes I have to run the script twice becasue nmap doesn't display all the devices. WTF nmap... Plz tell me what i'm doing wrong plz
+nm = nmap.PortScanner()
+nm.scan(hosts=target_ip, arguments="-sn -n")
 
 ubiquiti_devices = []
 
-for line in arp_output.splitlines():
-    for prefix in mac_prefixes:
-        if re.search(prefix, line, re.I):
-            ip_match = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
-            if ip_match:
-                ip = ip_match.group(0)
-                mac = re.search(r"([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}", line).group(0)
-                mac = re.sub(r"\s+dynamic|\s", "", mac)
-                ubiquiti_devices.append({
-                    "IPAddress": ip,
-                    "MACAddress": mac
-                })
+for host in nm.all_hosts():
+    if 'mac' in nm[host]['addresses']:
+        mac = nm[host]['addresses']['mac']
+        for prefix in mac_prefixes:
+            if mac.startswith(prefix):
+                ip = nm[host]['addresses']['ipv4']
+                ubiquiti_devices.append(ip)
+                print(f"{bcolors.OKBLUE}IP Address: {ip} | MAC Address: {mac}{bcolors.ENDC}")
                 break
 
 if len(ubiquiti_devices) == 0:
-    print(f"{bcolors.WARNING}No Ubiquiti devices found.){bcolors.ENDC}")
+    print(f"{bcolors.FAIL}No Ubiquiti devices found.{bcolors.ENDC}")
 else:
     print(f"{bcolors.OKGREEN}Found {len(ubiquiti_devices)} Ubiquiti Devices!{bcolors.ENDC}")
-    for device in ubiquiti_devices:
-        ip = device["IPAddress"]
-        mac = device["MACAddress"]
-        print(f"{bcolors.OKBLUE}IP Address: {ip}, MAC Address: {mac}{bcolors.ENDC}")
 
 run_set_inform = input("Do you want to run a set-inform command on all devices? (y/n): ")
 
@@ -144,14 +68,14 @@ if run_set_inform.lower() == "y":
     controller_ip = input(f"{bcolors.HEADER}Enter the IP address or FQDN of your UniFi controller: {bcolors.ENDC}")
 
     for device in ubiquiti_devices:
-        ip = device["IPAddress"]
+        ip = device
 
         try:
             ssh_client = paramiko.SSHClient()
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh_client.connect(ip, username=username, password=password)
 
-            time.sleep(.5)  # Add a delay of half a second
+            time.sleep(.5)  # Add a delay of half a second after connecting. (Slow ass Ubiquiti devices... /s)
 
             command = f"/usr/bin/mca-cli-op set-inform http://{controller_ip}:8080/inform"
             stdin, stdout, stderr = ssh_client.exec_command(command)
@@ -163,10 +87,18 @@ if run_set_inform.lower() == "y":
                 print(f"{bcolors.FAIL}Failed to send set-inform command to {ip}{bcolors.ENDC}")
 
             ssh_client.close()
+        except (paramiko.ssh_exception.SSHException, EOFError) as e:
+            if "Error reading SSH protocol banner" in str(e):
+                print(f"{bcolors.FAIL}Failed to connect to {ip}. This device likely doesn't support SSH.{bcolors.ENDC}")
+            else:
+                print(f"{bcolors.FAIL}Error connecting to {ip}: {str(e)}{bcolors.ENDC}")
         except Exception as e:
             print(f"{bcolors.FAIL}Error connecting to {ip}: {str(e)}{bcolors.ENDC}")
-print(f"{bcolors.BOLD}Thank you for using my script!{bcolors.ENDC}")
-print("With <3  - MaxBroome on GitHub")
+        finally:
+            ssh_client.close()
+
+print(f"{bcolors.HEADER}Thank you for using my script!{bcolors.ENDC}")
+print(f"With {bcolors.FAIL}<3{bcolors.ENDC} - MaxBroome on GitHub")
 time.sleep(3)
 print("Exiting Now! Bye!")
 time.sleep(5)
